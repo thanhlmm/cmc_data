885355922d913e50f0b77c6f4490a24c2d190f83
Thanh Le <mihthanh27@gmail.com>
2021-12-05 08:25:45 +0700

    Add sectors-components

---
+++ dbt/models/sectors-component.sql
@@ -0,0 +1,18 @@
+{% set sector_components = ["Lending & Borrowing","Web3","DeFi Index","Identity","Oracles","Yearn Partnerships","Derivatives","Asset Management","DeFi 2.0","Memes","Play To Earn","Yield Aggregator","Yield Farming","Galaxy Digital Portfolio","Stablecoin","Fan Token","Wrapped Tokens","DAO","AMM","Jobs","Filesharing","Protocol-Owned Liquidity","Interoperability","Synthetics","Tokenized Stock","Metaverse"] %}
+
+{{
+  config(
+    materialized='view'
+  )
+}}
+
+SELECT
+  timestamp,
+  {% for component in sector_components %}
+    SUM(marketcap) FILTER (WHERE name = '{{component}}') AS "{{component}}_marketCap",
+    SUM(marketVolume) FILTER (WHERE name = '{{component}}') AS "{{component}}_marketVolume",
+  {% endfor %}
+  sum(marketcap) as totalMarketCap
+FROM {{ ref('sectors') }} 
+GROUP BY timestamp
+ORDER BY timestamp
